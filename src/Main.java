0b6d87158f026584448f8728b0066974608d0a58
schedowka <marina_molodkina@proton.me>
2024-10-18 18:07:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 100;
+        while(n <= 200) {
+            if (n % 3 ==0) {
+                System.out.println(n);
+            }
+            n++;
+        }
+
+    }
+}
